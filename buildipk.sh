8270b53beb4ad488664d3b1e47ad55398da6e9b5
i3oot <tobias.waller@googlemail.com>
2014-08-16 23:33:30 +0200

    build script

--- buildipk.sh
+++ buildipk.sh
@@ -1,17 +1,8 @@
  #!/bin/sh
 
-rm -rf dist
-mkdir dist
-
 export version=`date +%Y%m%d%H%M%S`
+sed "s/%DATE%/$version/g" pkg/CONTROL/control.template > pkg/CONTROL/control
 
-rm -rf moonbow-backend/opt/moonbow-backend
-mkdir -p moonbow-backend/opt/moonbow-backend
-cp ../src/main/* moonbow-backend/opt/moonbow-backend
-sed "s/%DATE%/$version/g" moonbow-backend/CONTROL/control.template > moonbow-backend/CONTROL/control
-
-pushd dist
-../ipkg-build ../moonbow-backend .
-popd 
+./ipkg-build pkg .
 
 
